9b796d9f12baca02657a4c0fd9ea8784adcd0cb2
Fraser MacColl <fraser.a.maccoll@gmail.com>
2025-04-16 14:19:29 +0100

    Added simple credential managment to git

--- home/development/git.nix
+++ home/development/git.nix
@@ -13,9 +13,22 @@
 
     userName = "Fraser MacColl";
     userEmail = "fraser.a.maccoll@gmail.com";
+
+    extraConfig = {
+      # GitHub credentials
+      credential.helper = "manager";
+      credential.credentialStore = "plaintext";
+
+      # Automatically create branches on remote from new local
+      push = { autoSetupRemote = true; };
+    };
   };
 
   programs.lazygit = {
     enable = true;
   };
+
+  home.packages = with pkgs; [
+    git-credential-manager
+  ];
 }
