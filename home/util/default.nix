d1f0e7c29e6a7e2706e8672b08a99c498dac2a50
Fraser MacColl <fraser.a.maccoll@gmail.com>
2025-04-21 21:02:44 +0100

    Added dolphin config with generated config

--- home/util/default.nix
+++ home/util/default.nix
@@ -12,11 +12,11 @@
 #    ./rclone.nix
     # Having issues getting rclone working properly on startup, but
     # otherwise looks like it should work once I get that sorted.
+    ./dolphin.nix
   ];
 
   home.packages = with pkgs; [
     kdePackages.kcalc
-    kdePackages.dolphin
     libnotify
   ];
 
