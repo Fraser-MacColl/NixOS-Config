59a2f6dddbea3d2af23a3ed2214cf2eb7a1848c2
Fraser MacColl <fraser.a.maccoll@gmail.com>
2025-05-02 21:49:50 +0100

    Got Rclone *mostly* working, issues with fusermount permissions

--- home-modules/util/rclone.nix
+++ home-modules/util/rclone.nix
@@ -5,28 +5,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    coreutils
     rclone
-    fuse
   ];
-
-  # Config file is not auto generated, as it would then need to hold secrets, which shouldn't be added to the nix store.
-  # Instead, will a remote called "onedrive" once it exists in the default file location.
-  # Basically, on fresh install run through the standard "rclone config" setup, and in future it will be autostarted.
-  # Will probably need manually restarted on first addition however.
-
-  systemd.user.services.onedrive-mount = {
-    Unit = {
-      Description = "Onedrive automount for fraser managed by home manager.";
-      After = [ "network-online.target" ];
-    };
-    Service = {
-      Type = "notify";
-      # Causing issues with mkdir not existing
-#      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/Onedrive";
-      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes mount onedrive: %h/Onedrive";
-      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/Onedrive";
-    };
-    Install.WantedBy = [ "default.target" ];
-  };
 }
--- users/fraser/NixBox/default.nix
+++ users/fraser/NixBox/default.nix
@@ -1,10 +1,50 @@
 #
 # Default Home-Manager configs for fraser, specific to NixBox host
 #
-
+{ pkgs, ... }:
 {
-  # No specific host configuration needed, just fall back to the default
+  # Default for pretty much everything except below
   imports = [
     ../default.nix
   ];
+
+  # #=--------------=#
+  # |  RCLONE MOUNT  |
+  # #=--------------=#
+
+  home.packages = [ pkgs.fuse ];
+
+  # Config file is not auto generated, as it would then need to hold secrets, which shouldn't be added to the nix store.
+  # Instead, will a remote called "onedrive" once it exists in the default file location.
+  # Basically, on fresh install run through the standard "rclone config" setup, and in future it will be autostarted.
+  # Will probably need manually restarted on first addition however.
+
+  systemd.user.services.onedrive-mount = {
+    Unit = {
+      Description = "Onedrive automount for fraser managed by home manager.";
+      StartLimitIntervalSec = 70;
+      StartLimitBurst = 3;
+    };
+
+    Service = {
+      Type = "notify";
+      ExecStartPre = [
+        "${pkgs.coreutils}/bin/mkdir -p %h/Onedrive"
+        "${pkgs.coreutils}/bin/sleep 20" # Sleep 20 seconds before starting to let wifi connect
+      ];
+      ExecStart = "${pkgs.rclone}/bin/rclone --vfs-cache-mode writes mount onedrive: %h/Onedrive";
+      # This fusermount command consistently returning operation not permitted for some reason, despite manually running command working.
+      # Just commenting out for the time being to stop constant errors in journal, though it's not a clean shutdown anymore
+      # ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/Onedrive";
+
+      # In combination with the start limits above, this makes the unit try to restart 3 times
+      # spaced 20 seconds apart. If these fail it sends a notification
+      Restart = "on-failure";
+      RestartSec = "20";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
