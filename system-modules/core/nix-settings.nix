7a0deaad1f43b877d682469feb11ef5ce7097502
Fraser MacColl <fraser.a.maccoll@gmail.com>
2025-05-12 16:13:42 +0100

    Increase download buffer size to try and stop warning on large downloads

--- system-modules/core/nix-settings.nix
+++ system-modules/core/nix-settings.nix
@@ -19,6 +19,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
+    download-buffer-size = 128 * 1024 * 1024; # 128 MiB up from 64 MiB default
   };
 
   # do garbage collection weekly to keep disk usage low
